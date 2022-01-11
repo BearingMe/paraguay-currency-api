178af6456c3d1bcfb208feab7bbc640078f1ee69
BearingMe <gomes.bruno201@gmail.com>
2022-01-11 19:41:22 -0300

    feat: inicia o router

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,11 @@
-import cheerio from 'cheerio';
+import express from "express";
+import routes from "./routes";
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+// set up routes
+app.use("/api/v1", routes.api);
+app.get("/", (req, res) => res.redirect("/api/v1"));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
