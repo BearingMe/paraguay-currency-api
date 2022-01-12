3c27bdb131f06910e075ef93a21cfcd82e905bbf
BearingMe <gomes.bruno201@gmail.com>
2022-01-11 23:05:01 -0300

    feat: adiciona a view

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import "dotenv/config";
 import express from "express";
 import routes from "./routes";
+import path from "path";
 import db from "./db";
 
 const app = express();
 const port = process.env.PORT || 3000;
+const staticPath = path.join(__dirname, "./public");
+
+// load middlewares
+app.use(express.static(staticPath));
 
 // set up routes
 app.use("/api/v1", routes.api);
