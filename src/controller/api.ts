4557ec538e425862b2cc88956b2a8c696f1b8795
BearingMe <gomes.bruno201@gmail.com>
2022-01-11 21:51:43 -0300

    feat: adiciona funcionalidades ao projeto
    controllers agora permeiam o banco de dados e retornam os dados para
    o endpoint

--- src/controller/api.ts
+++ src/controller/api.ts
@@ -1,9 +1,23 @@
 import express from "express";
+import models from "../models";
+import services from "../services";
 
 export function getData(req: express.Request, res: express.Response) {
-  res.send("Hello World!");
+  models.api
+    .find()
+    .then((data) => res.json(data))
+    .catch((err) => res.status(500).json(err));
 }
 
 export function updateServer(req: express.Request, res: express.Response) {
-  res.send("Hello World!");
+  async function wrapper() {
+    const data = await services.scrapper.getData();
+
+    models.api
+      .updateMany({ _id: "61de129684db87fe6fbe36d5" }, data, { upsert: true })
+      .then(() => res.status(201).json("update successful"))
+      .catch((err) => res.send(err));
+  }
+
+  wrapper().catch((err) => res.status(500).send(err));
 }
